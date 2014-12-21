eef7b67ca947794451bb4db59f72f13ec2ee2418
mgp25 <me@mgp25.com>
2014-12-21 23:49:16 +0100

    Update WhatsConstants.cs

--- WhatsAppApi/Settings/WhatsConstants.cs
+++ WhatsAppApi/Settings/WhatsConstants.cs
@@ -42,7 +42,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The whatsapp version the client complies to
         /// </summary>
-        public const string WhatsAppVer = "2.11.452";
+        public const string WhatsAppVer = "2.11.473";
 
         /// <summary>
         /// The port that needs to be connected to
@@ -57,7 +57,7 @@ namespace WhatsAppApi.Settings
         /// <summary>
         /// The useragent used for http requests
         /// </summary>
-        public const string UserAgent = "WhatsApp/2.11.452 Android/4.0.4 Device/GalaxyS2";
+        public const string UserAgent = "WhatsApp/2.11.473 Android/4.0.4 Device/GalaxyS2";
 
         #endregion
 
